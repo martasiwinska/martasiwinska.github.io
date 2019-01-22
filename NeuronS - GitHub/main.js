1607186d920a2f1f748a07e4dc24bfa4edd60a72
martasiwinska <33381479+martasiwinska@users.noreply.github.com>
2019-01-22 17:31:26 +0100

    Add files via upload

---
+++ NeuronS - GitHub/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+// SCROLL-ICON
+
+    //     const ScrollBtn = $("scroll-button");
+    //     const ScrollBtnOffset = ScrollBtn.offset();
+    //     const ScrollBtnPosition = ScrollBtnOffset.top;
+    //     const downIconActivity = ScrollBtnPosition + window.innerHeight;
+    //     const topIconActivity = ScrollBtnPosition - window.innerHeight;
+
+    // $("#scroll-down").click(function(){
+    //         $("html, body").animate({ scrollTop: downIconActivity}, 600);
+    //         console.log(ScrollBtnPosition);
+    // });
+
+    // $("#scroll-top").click(function(){
+    //     console.log("działa");
+    //     $("html, body").animate({ scrollTop: topIconActivity}, 600);
+    // });
+
+
+// HEADER
+
+$(window).scroll(function() {
+
+        if((pageYOffset > 40)&&(window.innerWidth > 620)) {
+            $("#main-header").css("clip-path", "polygon(0 0, 100% 0, 100% 100%, 0 100%")
+            $("#main-header").css("transistion", "ease-in-out 8s");    
+        }
+         
+      });
+    
+});

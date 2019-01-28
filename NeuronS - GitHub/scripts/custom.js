83f87d1710f8c7cd142f401d7775d02e87756c2f
ka-wu <kawu24@op.pl>
2019-01-28 23:46:28 +0100

    poprawa

--- NeuronS - GitHub/scripts/custom.js
+++ NeuronS - GitHub/scripts/custom.js
@@ -2,46 +2,46 @@
 
 $('#duze1').hover(
 function() {
-$('#duze1').html('ipsum dolor adipisicing elit. Tempore repellat eveniet placeat animi, ipsam amet mollitia? Eum dicta voluptate a, repudiandae eligendi quod quibusdam blanditiis fugit omnis placeat. Commodi dolore, voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq');
+$('#duze1').html('ipsum dolor adipisicing elit. Tempore repellat eveniet placeat animi, ipsam amet mollitia? Eum dicta voluptate a, repudiandae eligendi quod quibusdam blanditiis fugit omnis placeat. Commodi dolore, voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  neque, quia, aspernatur incidunt earum laborum laudantiu ptatem ');
 }, // koniec mouseover
 function() {
-$('#duze1').html('<img src="img/projekt1.jpg" class="img-fluid" style="border-radius: 25px 25px 0px 0px; margin-bottom: 0.5rem"><small><h6 class="card-title" style="text-align:center">Nazwa aktualności 1</h6></small><p class="p-2" style="text-align: justify;">This is a longer card with supporting text below as a natui a lo bit</p>');
+$('#duze1').html('<img src="img/projekt1.jpg" class="img-fluid" style="border-radius: 25px 25px 0px 0px; margin-bottom: 0.5rem"></div><h6 class="card-title" style="text-align:center">Nazwa aktualności 2</h6></small><p class="p-2" style="text-align: justify;">This is a longer card with supporting text bThis is a longeas a natuelow as a natural lead-in to additional content. This content is a little bit</p>');
 } // koniec mouseout
 ); // koniec hover
 
 $('#duze2').hover(
 function() {
-$('#duze2').html('ipsum dolor adipisicing elit. Tempore repellat eveniet placeat animi, ipsam amet mollitia? Eum dicta voluptate a, repudiandae eligendi quod quibusdam blanditiis fugit omnis placeat. Commodi dolore, voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq');
+$('#duze2').html('ipsum dolor adipisicing elit. Tempore repellat eveniet placeat animi, ipsam amet mollitia? Eum dicta voluptate a, repudiandae eligendi quod quibusdam blanditiis fugit omnis placeat. Commodi dolore, voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  neque, quia, aspernatur incidunt earum laborum laudantiu ptatem ');
 }, // koniec mouseover
 function() {
-$('#duze2').html('<img src="img/projekt2.jpg" class="img-fluid" style="border-radius: 25px 25px 0px 0px; margin-bottom: 0.5rem"><small><h6 class="card-title" style="text-align:center">Nazwa aktualności 2</h6></small><p class="p-2" style="text-align: justify;">This is a longer card with supporting text below as a natui a lo bit</p>');
+$('#duze2').html('<img src="img/projekt2.jpg" class="img-fluid" style="border-radius: 25px 25px 0px 0px; margin-bottom: 0.5rem"></div><h6 class="card-title" style="text-align:center">Nazwa aktualności 2</h6></small><p class="p-2" style="text-align: justify;">This is a longer card with supporting text bThis is a longeas a natuelow as a natural lead-in to additional content. This content is a little bit</p>');
 } // koniec mouseout
 ); // koniec hover
 
 $('#duze3').hover(
 function() {
-$('#duze3').html(' ipsum dolor adipisicing elit. Tempore repellat eveniet placeat animi, ipsam amet mollitia? Eum dicta voluptate a, repudiandae eligendi quod quibusdam blanditiis fugit omnis placeat. Commodi dolore, voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq');
+$('#duze3').html('ipsum dolor adipisicing elit. Tempore repellat eveniet placeat animi, ipsam amet mollitia? Eum dicta voluptate a, repudiandae eligendi quod quibusdam blanditiis fugit omnis placeat. Commodi dolore, voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  neque, quia, aspernatur incidunt earum laborum laudantiu ptatem ');
 }, // koniec mouseover
 function() {
-$('#duze3').html('<img src="img/projekt3.jpg" class="img-fluid" style="border-radius: 25px 25px 0px 0px; margin-bottom: 0.5rem"><small><h6 class="card-title" style="text-align:center">Nazwa aktualności 3</h6></small><p class="p-2" style="text-align: justify;">This is a longer card with supporting text below as a natui a lo bit</p>');
+$('#duze3').html('<img src="img/projekt3.jpg" class="img-fluid" style="border-radius: 25px 25px 0px 0px; margin-bottom: 0.5rem"></div><h6 class="card-title" style="text-align:center">Nazwa aktualności 2</h6></small><p class="p-2" style="text-align: justify;">This is a longer card with supporting text bThis is a longeas a natuelow as a natural lead-in to additional content. This content is a little bit</p>');
 } // koniec mouseout
 ); // koniec hover
 
 $('#duze4').hover(
 function() {
-$('#duze4').html('ipsum dolor adipisicing elit. Tempore repellat eveniet placeat animi, ipsam amet mollitia? Eum dicta voluptate a, repudiandae eligendi quod quibusdam blanditiis fugit omnis placeat. Commodi dolore, voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq');
+$('#duze4').html('ipsum dolor adipisicing elit. Tempore repellat eveniet placeat animi, ipsam amet mollitia? Eum dicta voluptate a, repudiandae eligendi quod quibusdam blanditiis fugit omnis placeat. Commodi dolore, voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  neque, quia, aspernatur incidunt earum laborum laudantiu ptatem ');
 }, // koniec mouseover
 function() {
-$('#duze4').html('<img src="img/projekt4.jpg" class="img-fluid" style="border-radius: 25px 25px 0px 0px; margin-bottom: 0.5rem"><small><h6 class="card-title" style="text-align:center">Nazwa aktualności 4</h6></small><p class="p-2" style="text-align: justify;">This is a longer card with supporting text below as a natui a lo bit</p>');
+$('#duze4').html('<img src="img/projekt4.jpg" class="img-fluid" style="border-radius: 25px 25px 0px 0px; margin-bottom: 0.5rem"></div><h6 class="card-title" style="text-align:center">Nazwa aktualności 2</h6></small><p class="p-2" style="text-align: justify;">This is a longer card with supporting text bThis is a longeas a natuelow as a natural lead-in to additional content. This content is a little bit</p>');
 } // koniec mouseout
 ); // koniec hover
 
 $('#duze5').hover(
 function() {
-$('#duze5').html('ipsum dolor adipisicing elit. Tempore repellat eveniet placeat animi, ipsam amet mollitia? Eum dicta voluptate a, repudiandae eligendi quod quibusdam blanditiis fugit omnis placeat. Commodi dolore, voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq');
+$('#duze5').html('ipsum dolor adipisicing eamet mollitia? Eum dicta voluptate a, repudiandae eligendi quod quibusdam blanditiis fugit omnis placeat. Commodi dolore, voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  voluptatem neque, quia, aspernatur incidunt earum laborum laudantiu ptatem neq  neque, quia, aspernatur incidunt earum laborum laudantiu ptatem ');
 }, // koniec mouseover
 function() {
-$('#duze5').html('<img src="img/projekt5.jpg" class="img-fluid" style="border-radius: 25px 25px 0px 0px; margin-bottom: 0.5rem"><small><h6 class="card-title" style="text-align:center">Nazwa aktualności 4</h6></small><p class="p-2" style="text-align: justify;">This is a longer card with supporting text below as a natui a lo bit</p>');
+$('#duze5').html('<img src="img/projekt5.jpg" class="img-fluid" style="border-radius: 25px 25px 0px 0px; margin-bottom: 0.5rem"></div><h6 class="card-title" style="text-align:center">Nazwa aktualności 2</h6></small><p class="p-2" style="text-align: justify;">This is a longer card with supporting text bThis is a longeas a natuelow as a natural lead-in to additional content. This content is a little bit</p>');
 } // koniec mouseout
 ); // koniec hover
 
